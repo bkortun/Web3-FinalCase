88bfcf47d60e92e3f6542809b915b9e76aa79d16
bkortun <bariskorun@gmail.com>
2023-10-06 22:00:34 +0300

    Task2 was completed

--- contracts/RentalContract.sol
+++ contracts/RentalContract.sol
@@ -3,13 +3,17 @@
 pragma solidity  >=0.8.2 <0.9.0;
 
 contract Rental{
-    struct Agreement{
-        string hirerName;
-        string hirerAddress;
-        string renterName;
-        PropertyType propertyType;
-        string property;
-    }
+    //hirer => kiracı
+    //renter => mal sahibi
+
+    event SendReport(address indexed _user,Report report);
+
+    mapping (address=>Property) public propertiesOfUsers ;
+
+    mapping (uint=>Agreement) public agreements ;
+    uint[] public agreementIds;
+
+    Report[] reports ;
 
     enum PropertyType{
         Residential,
@@ -18,23 +22,75 @@ contract Rental{
 
     struct Property{
         string name;
+    }
+
+    struct Hirer{
+        address id;
+        string fullName;
+    }
+
+    struct Renter{
+        address id;
+        string fullName;
+    }
+
+
+    struct Tenancy{
+        Hirer hirer;
+        address tenancyAddress;
+        Renter renter;
+    }
+
+    struct Agreement{
+        uint id;
+        Tenancy tenancy;
         uint startDate;
-        uint endDate;
+        uint endDate;    
     }
 
-    function rent() isRenter pure public{
-
+    struct Report{
+        Hirer hirer;
+        Property property;
+        string subject;
+        string message;
+        uint createdDate;
     }
 
-    function finishRent() public{
-
+    modifier isHirer(){
+        _;
     }
 
     modifier isRenter(){
         _;
     }
+   
+    
 
-    function enterProperty() isRenter public{
+    function enterProperty(string memory propertyName) isRenter public{
+        Property memory _property;
+        _property.name=propertyName;
+        propertiesOfUsers[msg.sender] = _property;
+    }
 
+    function rent(Agreement memory agreement) isRenter  public{
+        agreements[agreement.id] = agreement;
+        agreementIds.push(agreement.id);
+    }
+
+    function finishRent(uint agreementId) isRenter  public{
+        delete agreements[agreementId];
+        
+        for (uint i = 0; i < agreementIds.length; i++) {
+            if (agreementIds[i] == agreementId) {
+                agreementIds[i] = agreementIds[agreementIds.length - 1];
+                agreementIds.pop();
+                break;
+            }
+        }
+    }
+
+    function sendReportToRenter(Report memory report) isHirer public{
+        emit SendReport(msg.sender,report);
+        reports.push(report);
     }
 }
